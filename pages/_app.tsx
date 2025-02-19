967e047404938eaadc9f9bff52934bb0d9bbd32f
robdongas <rob@dongas.me>
2025-02-19 13:50:08 +1100

    Add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,15 @@ import { VisualEditing } from '@sanity/visual-editing/next-pages-router'
 import { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 
+import { Inclusive_Sans } from "next/font/google"
+
+const sans = Inclusive_Sans({
+  variable: '--font-sans',
+  subsets: ['latin'],
+  weight: '400',
+})
+
+
 export interface SharedPageProps {
   draftMode: boolean
   token: string
@@ -17,7 +26,7 @@ export default function App({
 }: AppProps<SharedPageProps>) {
   const { draftMode, token } = pageProps
   return (
-    <>
+    <div className={`${sans.className} antialiased`}>
       {draftMode ? (
         <PreviewProvider token={token}>
           <Component {...pageProps} />
@@ -26,6 +35,6 @@ export default function App({
         <Component {...pageProps} />
       )}
       {draftMode && <VisualEditing />}
-    </>
+    </div>
   )
 }

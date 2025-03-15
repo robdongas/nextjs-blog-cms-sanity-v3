0bbbee8560028d4f326865ae08a4779766675ed4
robdongas <rob@dongas.me>
2025-03-15 14:05:38 +1100

    fix gtags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,13 +34,14 @@ export default function App({
       dataLayer.push(arguments);
     }
     gtag('js', new Date());
-    gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+    gtag('config', process.env.NEXT_PUBLIC_GA_ID);
+    console.log(window.dataLayer)
   }, []);
   const { draftMode, token } = pageProps
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -48,7 +49,7 @@ export default function App({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
         `}
       </Script>
       <div className={`${sans.className} antialiased`}>

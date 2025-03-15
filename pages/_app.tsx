c9dcc9626236ccc4d363096525ef09f659f4bd24
robdongas <rob@dongas.me>
2025-03-15 13:51:06 +1100

    add Google analytics tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import '../app/globals.css'
 import { VisualEditing } from '@sanity/visual-editing/next-pages-router'
 import { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
+import { useEffect } from 'react'
+import Script from 'next/script'
 
 import { Inclusive_Sans } from "next/font/google"
 
@@ -25,17 +27,40 @@ export default function App({
   Component,
   pageProps,
 }: AppProps<SharedPageProps>) {
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+  }, []);
   const { draftMode, token } = pageProps
   return (
-    <div className={`${sans.className} antialiased`}>
-      {draftMode ? (
-        <PreviewProvider token={token}>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <div className={`${sans.className} antialiased`}>
+        {draftMode ? (
+          <PreviewProvider token={token}>
+            <Component {...pageProps} />
+          </PreviewProvider>
+        ) : (
           <Component {...pageProps} />
-        </PreviewProvider>
-      ) : (
-        <Component {...pageProps} />
-      )}
-      {draftMode && <VisualEditing />}
-    </div>
+        )}
+        {draftMode && <VisualEditing />}
+      </div>
+    </>
   )
 }

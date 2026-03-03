c93fd8428e565913a1ff7a8c88a7f35e98284686
robdongas <rob@dongas.me>
2026-03-03 14:31:51 +1100

    refactor: switch from next to astro for faster builds

---
+++ src/components/CursorFollower.tsx
@@ -0,0 +1,74 @@
+import { useRef, useEffect } from 'react'
+import gsap from 'gsap'
+
+export default function CursorFollower() {
+    const cursorRef = useRef<HTMLDivElement | null>(null)
+    const tl = useRef<gsap.core.Timeline | null>(null)
+
+    useEffect(() => {
+        const cursor = cursorRef.current
+
+        if (!cursor) return
+
+        // Set initial opacity to 0
+        gsap.set(cursor, { opacity: 0 })
+
+        // Initialize the GSAP timeline
+        tl.current = gsap.timeline({ paused: true })
+
+        // Add animations to the timeline
+        tl.current.to(cursor, {
+            opacity: 1,
+            duration: 0.5,
+            ease: "power2.out",
+        })
+            .to(cursor, {
+                scale: 1.5,
+                duration: 0.3,
+                ease: "power2.out",
+            }, "<")
+
+        const handleMouseMove = (e: MouseEvent) => {
+            const { clientX, clientY, target } = e
+
+            gsap.to(cursor, {
+                x: clientX,
+                y: clientY,
+                duration: 0.1,
+                ease: "power2.out",
+            })
+
+            if (cursor.style.opacity === '0') {
+                tl.current.play()
+            }
+
+            const isTargetLinkOrBtn =
+                target instanceof HTMLElement && (target.closest('a') || target.closest('button'))
+
+            gsap.to(cursor, {
+                opacity: isTargetLinkOrBtn ? 0 : 1,
+                scale: isTargetLinkOrBtn ? 2.5 : 1,
+                duration: 0.3,
+                ease: "power2.out",
+            })
+        }
+
+        const handleMouseLeave = () => {
+            gsap.to(cursor, {
+                opacity: 0,
+                duration: 0.5,
+                ease: "power2.out",
+            })
+        }
+
+        window.addEventListener('mousemove', handleMouseMove)
+        window.addEventListener('mouseleave', handleMouseLeave)
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove)
+            window.removeEventListener('mouseleave', handleMouseLeave)
+        }
+    }, [])
+
+    return <div ref={cursorRef} className='cursor-follower' />
+}
---
+++ src/components/ProjectPreview.tsx
@@ -0,0 +1,48 @@
+import { urlForImage } from '../lib/sanity.image'
+import type { Project } from '../lib/sanity.queries'
+
+export default function ProjectPreview({
+    title,
+    projectType,
+    coverImage,
+    coverHidden,
+    date,
+    excerpt,
+    slug,
+}: Omit<Project, '_id'>) {
+    const hasImage = coverImage?.asset?._ref
+    const imageUrl = hasImage ? urlForImage(coverImage).url() : null
+    const width = coverImage?.dimensions?.width || 2000
+    const height = coverImage?.dimensions?.height || 1000
+
+    return (
+        <div {...{ "project-type": `${projectType}` }}>
+            <div className="relative group">
+                <div className="relative">
+                    {hasImage ? (
+                        <a href={`/projects/${slug}`} aria-label={title}>
+                            <img
+                                className="h-auto w-full"
+                                src={imageUrl}
+                                alt=""
+                                width={width}
+                                height={height}
+                                loading="lazy"
+                            />
+                        </a>
+                    ) : (
+                        <div style={{ paddingTop: '50%', backgroundColor: '#ddd' }} />
+                    )}
+                </div>
+                <div className='absolute pointer-events-none inset-0 bg-gradient-to-b from-transparent to-black/60 opacity-0 transition-opacity duration-300 group-hover:opacity-100'></div>
+                <div className='absolute bottom-4 left-4 right-4 opacity-0 transform translate-y-2 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0'>
+                    <h3 className="text-3xl leading-snug text-balance text-white">
+                        <a href={`/projects/${slug}`}>
+                            {title}
+                        </a>
+                    </h3>
+                </div>
+            </div>
+        </div>
+    )
+}
